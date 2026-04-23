912ac05fd3982e9737817627a679048ac867d6b9
t0ngdrumz <tingjake@gmail.com>
2026-04-23 17:22:47 -0400

    Update config.h

--- keyboards/JT75/config.h
+++ keyboards/JT75/config.h
@@ -1,1 +1,14 @@
+#pragma once
 
+#define MATRIX_ROWS 6
+#define MATRIX_COLS 18
+
+#define DIODE_DIRECTION COL2ROW
+
+// Split keyboard
+#define SPLIT_KEYBOARD
+#define MASTER_LEFT
+
+// Communication between halves (simple serial split)
+#define USE_SERIAL
+#define SOFT_SERIAL_PIN GP1
