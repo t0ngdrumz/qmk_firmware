165fa9e04fbc7f2c3601bdda07e466618c2b1342
t0ngdrumz <tingjake@gmail.com>
2026-04-24 02:31:33 -0400

    Update config.h

--- keyboards/JT75/config.h
+++ keyboards/JT75/config.h
@@ -2,17 +2,14 @@
 
 #define EE_HANDS
 
-// Split keyboard
 #define SPLIT_KEYBOARD
 
-// TRRS single-wire serial on GP15
+// TRRS single-wire split communication (your GP15)
 #define SOFT_SERIAL_PIN GP15
 
-// Matrix size (6 rows per half)
 #define MATRIX_ROWS 12
 #define MATRIX_COLS 9
 
 #define DIODE_DIRECTION COL2ROW
 
-// Helps RP2040 split stability
 #define SPLIT_USB_DETECT
