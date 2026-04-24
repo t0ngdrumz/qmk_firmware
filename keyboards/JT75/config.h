57e0bb8910b421c1dfa4322d698ea3eca65d5626
t0ngdrumz <tingjake@gmail.com>
2026-04-24 00:20:40 -0400

    Update config.h

--- keyboards/JT75/config.h
+++ keyboards/JT75/config.h
@@ -1,13 +1,17 @@
 #pragma once
 
 #define MATRIX_ROWS 6
-#define MATRIX_COLS 18
+#define MATRIX_COLS 9
+
+#define MATRIX_ROW_PINS { GP9, GP10, GP11, GP12, GP13, GP14 }
+#define MATRIX_COL_PINS { GP8, GP7, GP6, GP5, GP4, GP3, GP2, GP1, GP0 }
 
 #define DIODE_DIRECTION COL2ROW
 
-// --- SPLIT SETTINGS (TRRS SERIAL) ---
+// Split settings
 #define SPLIT_KEYBOARD
-#define USE_SERIAL
-#define SOFT_SERIAL_PIN GP1
-
 #define MASTER_LEFT
+
+// TRRS serial link
+#define USE_SERIAL
+#define SOFT_SERIAL_PIN GP15
